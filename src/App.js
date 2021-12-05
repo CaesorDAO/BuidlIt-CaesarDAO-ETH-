5f84a96b211087560c75d48db3e452338c1e276e
Disha Agarwalla <dishaagarwalla2001@gmail.com>
2021-12-05 20:16:03 +0530

    access levels added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import ProtectedRoute from "./utils/ProtectedRoute";
+
 import Navbars from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -15,6 +17,13 @@ import Web3Modal from "web3modal";
 import web3 from "./ethereum/web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import SellGallery from "./components/SellGallery";
+
+import Segment from "./pages/Segment";
+import ContentPage from "./pages/ContentPage";
+
+import CaesarNFT from "./ethereum/CaesarNFT";
+
+import axios from "axios";
 
 const infuraId =
   "https://rinkeby.infura.io/v3/97c2d52095a84da7a0b710a8daa16acf";
@@ -37,6 +46,10 @@ let provider;
 
 const App = () => {
   const [account, setaccount] = useState("");
+
+  const [gold, setGold] = useState(false);
+  const [silver, setSilver] = useState(false);
+  const [bronze, setBronze] = useState(false);
 
   const onConnectWallet = async () => {
     console.log("connecting wallet...");
@@ -100,12 +113,59 @@ const App = () => {
     onConnectWallet();
   }, []);
 
+  const nftContractAddress = "0xD58d8C5274483B761BB375243F1cC88adBa151C5";
+
+  useEffect(() => {
+    const run = async () => {
+      try {
+        setGold(false);
+        setSilver(false);
+        setBronze(false);
+        const accounts = await web3.eth.getAccounts();
+        // setaccount(account[0]);
+
+        const userAddress = accounts[0];
+        // "0x6ff9c8ed337de934e46e773f61a1a3369617c3ce";
+        //   "0x5908bfd84673974ddb8b6688501a53ac5fc92b6b";
+        const balance = await CaesarNFT.methods
+          .balanceOf(userAddress.toString())
+          .call();
+
+        for (let i = 0; i < balance; i++) {
+          const tokenId = await CaesarNFT.methods
+            .tokenOfOwnerByIndex(userAddress, i)
+            .call();
+
+          const url =
+            // `https://api.opensea.io/api/v1/asset/0x2E9983b023934e72e1E115Ab6AEbB3636f1C4Cbe/${tokenId}/`;
+            `https://rinkeby-api.opensea.io/api/v1/asset/${nftContractAddress}/${tokenId}/`;
+          const { data } = await axios.get(url);
+
+          await data.traits.map((trait) => {
+            //   console.log(trait);
+            if (trait.trait_type === "Level") {
+              console.log(trait.value);
+              if (trait.value === "Gold") setGold(true);
+              else if (trait.value === "Silver") setSilver(true);
+              else if (trait.value === "Bronze") setBronze(true);
+            }
+            return 0;
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    run();
+  }, [account]);
+
   return (
     <div className="app-container">
       <Navbars
         account={account}
         onConnectWallet={onConnectWallet}
         onDisconnect={onDisconnect}
+        level={{ gold: gold, silver: silver, bronze: bronze }}
       />
       <Switch>
         <Route exact path="/" component={() => <Minting />} />
@@ -141,6 +201,45 @@ const App = () => {
           path="/staking"
           component={() => <Staking account={account} />}
         />
+
+        <ProtectedRoute
+          level={gold}
+          exact
+          path="/Gold"
+          component={() => <Segment segment="Gold" />}
+        />
+        <ProtectedRoute
+          level={gold}
+          exact
+          path="/Gold/:id"
+          component={() => <ContentPage segment="Gold" />}
+        />
+        <ProtectedRoute
+          level={silver}
+          exact
+          path="/Silver"
+          component={() => <Segment segment="Silver" />}
+        />
+        <ProtectedRoute
+          level={silver}
+          exact
+          path="/Silver/:id"
+          component={() => <ContentPage segment="Silver" />}
+        />
+        <ProtectedRoute
+          level={bronze}
+          exact
+          path="/Bronze"
+          component={() => <Segment segment="Bronze" />}
+        />
+        <ProtectedRoute
+          level={bronze}
+          exact
+          path="/Bronze/:id"
+          component={() => <ContentPage segment="Bronze" />}
+        />
+
+        <Route path="*" component={() => "404 NOT FOUND"} />
       </Switch>
       {/* <Footer /> */}
     </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import { Navbar, Container, Button } from "react-bootstrap";
 import Image from "../../images/image.png";
 import "./style.css";
 
-const Navbars = ({ account, onConnectWallet, onDisconnect }) => {
+const Navbars = ({ account, onConnectWallet, onDisconnect, level }) => {
   const address = account
     ? account.slice(0, 8) + "...." + account.slice(-8)
     : null;
@@ -18,31 +18,77 @@ const Navbars = ({ account, onConnectWallet, onDisconnect }) => {
           </Link>
         </Navbar.Brand>
 
-        <div className="gallery-nav">
-          <Link
-            // to="/"
-            to="/gallery"
-          >
-            Marketplace
+        <div
+          className="gallery-nav"
+          style={{
+            display: "grid",
+            gap: "20px",
+            border: "1px solid white",
+            borderRadius: "10px",
+            padding: "10px",
+          }}
+        >
+          <div className="gallery-nav">
+            <Link
+              // to="/"
+              to="/gallery"
+            >
+              Marketplace
+            </Link>
+            {/* <div className="hide">Coming soon</div> */}
+          </div>
+
+          <div className="gallery-nav">
+            <Link
+              // to="/"
+              to="/mint"
+            >
+              Mint
+            </Link>
+            {/* <div className="hide">Coming soon</div> */}
+          </div>
+
+          <div className="gallery-nav">
+            <Link to="/Staking">Staking</Link>
+            {/* <div className="hide">Coming soon</div> */}
+          </div>
+        </div>
+
+        <div
+          className="gallery-nav"
+          style={{
+            display: "grid",
+            gap: "20px",
+            border: "1px solid white",
+            borderRadius: "10px",
+            padding: "10px",
+          }}
+        >
+          <Link to="/Gold" style={{ color: level.gold ? "white" : "grey" }}>
+            Gold
           </Link>
-          {/* <div className="hide">Coming soon</div> */}
-        </div>
-
-        <div className="gallery-nav">
-          <Link
-            // to="/"
-            to="/mint"
-          >
-            Mint
+          <Link to="/Silver" style={{ color: level.silver ? "white" : "grey" }}>
+            Silver
           </Link>
-          {/* <div className="hide">Coming soon</div> */}
+          <Link to="/Bronze" style={{ color: level.bronze ? "white" : "grey" }}>
+            Bronze
+          </Link>
+          {/* {level.gold ? (
+            <Link to="/Gold" style={{ color: level.gold ? "white" : "grey" }}>
+              Gold
+            </Link>
+          ) : null}
+          {level.silver ? (
+            <Link to="/Silver" style={{ color: level.gold ? "white" : "grey" }}>
+              Silver
+            </Link>
+          ) : null}
+          {level.bronze ? (
+            <Link to="/Bronze" style={{ color: level.gold ? "white" : "grey" }}>
+              Bronze
+            </Link>
+          ) : null} */}
         </div>
-
-        <div className="gallery-nav">
-          <Link to="/Staking">Staking</Link>
-          {/* <div className="hide">Coming soon</div> */}
-        </div>
-
 
         {account ? (
           <Button className="test2" onClick={onDisconnect}>
